d5b2ebc66d8261ce18fa4a20eaefa03057560233
Seth Hoong <hoongseth@gmail.com>
2025-09-26 10:24:43 -0400

    Use vite dev server for testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.js";
 import "./style.css";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
